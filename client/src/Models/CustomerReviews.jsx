37dd7e1bdec3fc7b41167104b45d50e157c48527
anton10Luna <antonio.gilluna@gmail.com>
2021-03-16 01:48:28 -0700

    change to sample data comment

--- client/src/Models/CustomerReviews.jsx
+++ client/src/Models/CustomerReviews.jsx
@@ -78,7 +78,7 @@ export default CustomerReviews;
 //       title: 'not so bad',
 //       date: "2019-08-13T17:51:35.537Z",
 //       verifiedPurchase: true,
-//       text: 'Fugiat eius voluptatem asperiores ducimus accusantium vitae quia esse. Tenetur quia sunt fuga. Quia eaque et. Cumque nihil possimus facilis porro alias. Maxime eum et qui nisi quidem alias. Et harum voluptatum perferendis rerum nostrum. Voluptatem sunt nesciunt culpa accusantium ut debitis ipsa est quod. Quos dolor nisi. Totam quia et. Quo ullam maiores et repudiandae aut qui sunt quos explicabo. Temporibus necessitatibus quasi. Et eveniet rerum ratione perspiciatis commodi nulla cumque et. Omnis temporibus nesciunt eius. Soluta provident culpa ut consequuntur nostrum facere fugit voluptas omnis.',
+//       text: String,
 //       images: [],
 //       helpfulClickCount: 18
 //     }
